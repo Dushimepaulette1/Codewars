0443fa279defa1dcfcabd9264fb7dc1c4cf9cc23
Dushime Paulette <169043690+Dushimepaulette1@users.noreply.github.com>
2026-07-04 21:21:50 +0200

    Add solution - CodeHub

---
+++ 8-kyu/remove-duplicates-from-list/remove-duplicates-from-list.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq;
+​
+namespace Solution
+{
+  public static class Program
+  {
+    public static int[] distinct(int[] a)
+    {
+      return a.Distinct().ToArray();
+    }
+  }
+}
