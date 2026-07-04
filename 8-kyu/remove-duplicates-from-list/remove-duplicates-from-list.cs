e5f51c52bf715c0371d179111833f322d8ca5695
Dushime Paulette <169043690+Dushimepaulette1@users.noreply.github.com>
2026-07-04 21:36:34 +0200

    Update solution - CodeHub

--- 8-kyu/remove-duplicates-from-list/remove-duplicates-from-list.cs
+++ 8-kyu/remove-duplicates-from-list/remove-duplicates-from-list.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Collections.Generic;
 ​
 namespace Solution
 {
@@ -7,7 +8,15 @@ namespace Solution
   {
     public static int[] distinct(int[] a)
     {
-      return a.Distinct().ToArray();
+        List<int> final = new List<int>();
+        for(int i=0; i<a.Length; i++)
+        {
+            if(!final.Contains(a[i]))
+            {
+                final.Add(a[i]);
+            }
+        }
+      return final.ToArray();
     }
   }
 }
